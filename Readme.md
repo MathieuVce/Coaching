c10f963e54bab19eb59e1a14e89c2df40ab5fefa
Loic Lissens <loic.lissens@renewex.co>
2021-12-20 11:54:49 +0100

    Init front repo and firebase app
    Signed-off-by: Loic Lissens <loic.lissens@renewex.co>

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
 Vacance_M#9864
 mathieu.vacance@epitech.eu
+coaching.extia@gmail.com -> firebase app (projet crée)
---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <p>Coaching</p>
+    </main>
+  )
+}
+
+export default App
