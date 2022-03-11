1219e5c00e840ee234e13a88d1df3968a7cd8cc9
Mathieu VACANCE <mathieu.vacance@epitech.eu>
2022-03-11 17:35:29 +0100

    Mathieu VACANCE/ sticky navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FunctionComponent = () => {
 
   return(
       <>
-        <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue mb-3">
+        <nav className="sticky top-0 z-100 flex flex-wrap items-center justify-between px-2 py-3 bg-blue">
           <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between items-center lg:w-auto lg:static lg:block lg:justify-start">
               <div
